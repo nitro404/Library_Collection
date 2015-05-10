2bf09aeca8508da85594a4c8b60720e628286e1b
Kevin Scroggins <nitro404@gmail.com>
2015-05-10 16:30:43 -0400

    Simplified Math namespace usage

--- Math Library/C++/Source/Math/ExtendedMath.cpp
+++ Math Library/C++/Source/Math/ExtendedMath.cpp
@@ -1,11 +1,15 @@
 #include "Math/ExtendedMath.h"
 
-const float Math::PI = 3.14159265358979323846f;
-
-float Math::radiansToDegrees(float r) {
-	return r * (180.0f / PI);
-}
-
-float Math::degreesToRadians(float d) {
-	return d * (PI / 180.0f);
+namespace Math {
+	
+	const float PI = 3.14159265358979323846f;
+	
+	float radiansToDegrees(float r) {
+		return r * (180.0f / PI);
+	}
+	
+	float degreesToRadians(float d) {
+		return d * (PI / 180.0f);
+	}
+	
 }
--- Math Library/C++/Source/Math/ExtendedMath.h
+++ Math Library/C++/Source/Math/ExtendedMath.h
@@ -2,10 +2,12 @@
 #define EXTENDED_MATH_H
 
 namespace Math {
+	
 	extern const float PI;
-
+	
 	float radiansToDegrees(float r);
 	float degreesToRadians(float d);
+
 }
 
 #endif // EXTENDED_MATH_H
